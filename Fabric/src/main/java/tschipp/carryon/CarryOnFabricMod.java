cae5a2278757f19fc8abba70b6c788c0f376c2de
Nathan Finch <nathanrfinch@protonmail.com>
2025-09-09 04:39:06 +0100

    Fix respawn crash in #839
    I've made this patch for my own use and decided to submit it. I'm not very familiar with Minecraft modding, but this seems to do the trick.

--- Fabric/src/main/java/tschipp/carryon/CarryOnFabricMod.java
+++ Fabric/src/main/java/tschipp/carryon/CarryOnFabricMod.java
@@ -39,7 +39,7 @@ public class CarryOnFabricMod implements ModInitializer {
             builder -> builder
                     .initializer(() -> new CarryOnData(new CompoundTag()))
                     .persistent(CarryOnData.CODEC)
-                    .syncWith(CarryOnData.STREAM_CODEC, (t, p) -> p.connection != null)
+                    .syncWith(CarryOnData.STREAM_CODEC, (t, p) -> p.connection != null && !p.isRemoved())
                     .copyOnDeath()
 
     );
