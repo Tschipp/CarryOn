f91023af5d88b1df69b3102a6d1fc4cbb6439bd0
Tschipp <11649893+Tschipp@users.noreply.github.com>
2026-07-25 15:53:12 +0200

    Fixed SizeHelper type mismatch

--- Common/src/main/java/tschipp/carryon/utils/SizeHelper.java
+++ Common/src/main/java/tschipp/carryon/utils/SizeHelper.java
@@ -34,8 +34,8 @@ public class SizeHelper
 	{
 		if (!Constants.COMMON_CONFIG.settings.relativePlayerScale)
 			return 1;
-		double result = player.getAttributeBaseValue(Attributes.SCALE) / Math.max(player.getAttributeValue(Attributes.SCALE), 0.01);
-		return (float) result;
+		double actualScale = Math.max(player.getAttributeValue(Attributes.SCALE), 0.001);
+        return 1 / (float) actualScale;
 	}
 
 	public static float getEntityWidth(Entity entity)
@@ -78,7 +78,7 @@ public class SizeHelper
 
 	public static float getRelativeEntityArea(Player player, Entity entity)
 	{
-		double playerScaleFactor = getPlayerScaleFactor(player);
+		float playerScaleFactor = getPlayerScaleFactor(player);
 		return getEntityHeight(entity) * getEntityWidth(entity) * playerScaleFactor * playerScaleFactor;
 	}
 }
